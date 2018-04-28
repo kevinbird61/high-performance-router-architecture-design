83fc22149823710910211f842bbf4b04798ae182
kevinbird61 <kevinbird61@gmail.com>
2018-04-28 12:26:55 +0800

    Add ipv6 processing.
    * read from file.
    * convert from string.
    
    Prepare to implement binary trie.

--- bgp_ipv6/binary_trie/main.c
+++ bgp_ipv6/binary_trie/main.c
@@ -1,22 +1,82 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 #define IPV4 32
 #define IPV6 128
 
+typedef unsigned __int128 uint128_t;
+
 typedef struct node {
-    int id;
+    unsigned int port;
     struct node *left,*right;
 } NODE;
 
 typedef struct iptable {
-    int count;
+    unsigned int count;
 } IPTABLE;
+
+typedef struct entry {
+    uint128_t ipv6;
+    unsigned char len;
+    unsigned char port;
+} ENTRY;
+
+// global variables
+NODE *root;
+unsigned int num_entries=0;
+
+int hex2int(char c){
+    if(c>=48 && c<=57)
+        return c-'0';
+    else if(c>=97){
+        return c-'a'+10;
+    }
+    else if(c>=65 && c<=90){
+        return c-'A'+10;
+    }
+}
+
+void read_ip_info(char *str,uint128_t *ipv6,int *len,unsigned int *nexthop){
+    // read from str, and then store them into the other
+    // ip
+    // len
+    // nexthop
+    const char tok[]=":";
+    // ipv6 = 128 bits
+    char *buf;
+    // each piece will take 16 bits
+    int index=0;
+    unsigned long pieces[8]={0,0,0,0,0,0,0,0};
+
+    buf = strtok(str,tok);
+    while(buf!=NULL){
+        // store into pieces
+        if(strlen(buf)==0)
+            pieces[index]=0;
+        else{
+            // convert hex to dec
+            for(int i=0;i<4;i++){
+                pieces[index]+=(hex2int(buf[i])*pow(16,3-i));
+            }
+        }
+        index++;
+        // cut
+        buf = strtok(NULL,tok);
+    }
+
+    // After cutting, restore then into ipv6
+    for(int i=0;i<7;i++){
+        *ipv6+=pieces[i];
+        *ipv6<<=16;
+    }
+
+}
 
 int main(void){
     // root
-    NODE *root = NULL;
     root = (NODE *)malloc(sizeof(NODE*));
     IPTABLE *counter = NULL;
     counter = (IPTABLE *)malloc(129*sizeof(IPTABLE*));
@@ -33,6 +93,7 @@ int main(void){
 
     // processing
     char line[1024];
+    num_entries=0;
     while(fgets(line,1024,fp)!=NULL){
         //printf("%s\n",line);
         char *value = strtok(line,"/");
@@ -40,12 +101,24 @@ int main(void){
         // TODO: implement insert method, to record the value into this block
         counter[atoi(key)].count++;
         // printf("%s, %s\n",value,key);
+        uint128_t ipv6=0;
+        int len;
+        unsigned int nexthop;
+        printf("source: %s\n",value);
+        read_ip_info(value,&ipv6,&len,&nexthop);
+        //printf("ipv6 size: %d\n",(int)sizeof(ipv6));
+        for(int i=0;i<IPV6;i++){
+            if(i%16==0)
+                puts(" ");
+            printf("%d",(ipv6>>(IPV6-i-1) & 0x1));
+        }
+        printf("\n");
     }
 
     // Print debug
-    for(int i=0;i<=IPV6;i++){
+    /*for(int i=0;i<=IPV6;i++){
         printf("[%d] - %d\n",i,counter[i].count);
-    }
+    }*/
 
     // fclose(fp);
     return 0;
